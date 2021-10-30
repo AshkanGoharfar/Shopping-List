30ddbbf1501906898b5b2f5a4872a0ae0f3c518f
Ashkan Goharfar <ashkan.goharfar@gmail.com>
2021-10-31 00:01:26 +0330

    Issue of wrong portrait using vary of traits has been fixed.

--- Shopping-List/ViewController.swift
+++ Shopping-List/ViewController.swift
@@ -87,7 +87,47 @@ class ViewController: UIViewController {
     }
     
     
+    @IBAction func StepperNumItem_Pressed_1(_ sender: UIStepper) {
+        if (flagCancelPressedStepper_1 == "1"){
+            previousStepperValue_1 = Int(sender.value)
+            flagCancelPressedStepper_1 = "0"
+        }
+        NumOfItemLabel_1.text = String(Int(sender.value) - previousStepperValue_1 + 1)
+    }
     
+    
+    @IBAction func StepperNumItem_Pressed_2(_ sender: UIStepper) {
+        if (flagCancelPressedStepper_2 == "1"){
+            previousStepperValue_2 = Int(sender.value)
+            flagCancelPressedStepper_2 = "0"
+        }
+        NumOfItemLabel_2.text = String(Int(sender.value) - previousStepperValue_2 + 1)    }
+    
+    @IBAction func StepperNumItem_Pressed_3(_ sender: UIStepper) {
+        if (flagCancelPressedStepper_3 == "1"){
+            previousStepperValue_3 = Int(sender.value)
+            flagCancelPressedStepper_3 = "0"
+        }
+        NumOfItemLabel_3.text = String(Int(sender.value) - previousStepperValue_3 + 1)
+    }
+    
+    @IBAction func StepperNumItem_Pressed_4(_ sender: UIStepper) {
+        if (flagCancelPressedStepper_4 == "1"){
+            previousStepperValue_4 = Int(sender.value)
+            flagCancelPressedStepper_4 = "0"
+        }
+        NumOfItemLabel_4.text = String(Int(sender.value) - previousStepperValue_4 + 1)
+    }
+    
+    @IBAction func StepperNumItem_Pressed_5(_ sender: UIStepper) {
+        if (flagCancelPressedStepper_5 == "1"){
+            previousStepperValue_5 = Int(sender.value)
+            flagCancelPressedStepper_5 = "0"
+        }
+        NumOfItemLabel_5.text = String(Int(sender.value) - previousStepperValue_5 + 1)
+    }
+    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
